543970cd7bb36b84fd2580ff39760c032de8d7ae
Christopher McIlgrew <c.mcilgrew1982@gmail.com>
2021-03-16 13:03:20 +0000

    added few more files

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+# Create your tests here.
+class TestDjango(TestCase):
+
+    def test_is_this_thing_on(self):
+        '''
+        Simple test to ensure test is working
+        '''
+        self.assertEquals(1, 1)
