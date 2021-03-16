01556cca659f95872fe44db629bc0abec7af73c6
Christopher McIlgrew <c.mcilgrew1982@gmail.com>
2021-03-16 10:57:33 +0000

    changed spelling mistake in env.py

--- unicorn_attractor_new/settings.py
+++ unicorn_attractor_new/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('GITPOD_HOSTNAME'),
+ALLOWED_HOSTS = os.environ.get('GITPOD_HOSTNAME'), 'my-project-4-new.herokuapp.com'
 
 
 # Application definition
