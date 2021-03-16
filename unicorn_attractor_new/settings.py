5d7f8ef76b03803ec391550d0d8897ddce57e35a
Christopher McIlgrew <c.mcilgrew1982@gmail.com>
2021-03-16 13:43:04 +0000

    *

--- unicorn_attractor_new/settings.py
+++ unicorn_attractor_new/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get('GITPOD_HOSTNAME'), 'my-project-4-new.herokuapp.com'
 
